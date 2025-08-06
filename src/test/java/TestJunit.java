397790d428b3e2ecb85166eafe3477d4ecc011ce
nassai <hei.nassai@gmail.com>
2025-08-06 15:25:46 +0300

    add all files

---
+++ src/test/java/TestJunit.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestJunit {
+    @Test
+    public void test01(){
+        Assertions.assertEquals(2, 1 +1);
+    }
+}
